f02dac52c01fc25f84ffed137f654730dd5d1221
mehmettalhakaya <250542009@firat.edu.tr>
2025-12-03 21:45:11 +0300

    Update Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -11,9 +11,14 @@ public class Exercise4_4 {
     public static void main(String[] args) {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
+        // Bu satır derlenir ve çalışır, fakat getNumber()'ın döndürdüğü 42 değeri
+        // hiçbir değişkende tutulmadığı için "kullanılmaz". Programda görünür bir etkisi yoktur.
         getNumber();
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
+        // Aşağıdaki satır DERLEME HATASI verir çünkü sayHello() bir değer döndürmez (void).
+        // Bu yüzden System.out.println içinde bir değermiş gibi kullanılamaz.
+        // Örnek hata: "void type not allowed here"
         // System.out.println(sayHello() + 7);
 
         // Cevaplarinizi yorum olarak ekleyin.
